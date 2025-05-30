21605765af5660597cc4f995e9afac6f8b802230
Luzpiscapisca <geovana.luz@estudante.ifmt.edu.br>
2025-05-30 11:36:57 -0300

    VarArgs

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,19 @@
+package aplicacao;
+
+import entidades.ServidorPublico;
+
+public class Programa {
+    public static void main(String[] args) {
+        ServidorPublico isabela = new ServidorPublico();
+        isabela.setNome("Isabela");
+        isabela.setCargo("Auditor");
+        isabela.setOrgao("ANVISA");
+        isabela.setLotacao("Brasilia");
+        isabela.setEmail("isabela@gmail.");
+        
+        isabela.setSalario
+
+        System.out.println("Servidor: "+ isabela.getNome());
+        System.out.println("Salário total: "+isabela.calcularSalarioServidor());
+    }
+}
